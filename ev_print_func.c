a0264de3f7e8c6f45eb8adc8630f63d72cff6c0d
Ocen Chris <43829976+chrisjrocen@users.noreply.github.com>
2022-04-19 20:41:11 +0300

    Update ev_print_func.c

--- ev_print_func.c
+++ ev_print_func.c
@@ -4,7 +4,7 @@
  * ev_print_func - returns the amount of identifiers.
  * @s: argument indentifier
  * @index: index of argument identifier.
- * Return: amount of identifiers.
+ * Return: the amount of identifiers.
  */
 int ev_print_func(const char *s, int index)
 {
