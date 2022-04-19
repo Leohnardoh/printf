65bb9c3c72bff3bc1e2ee613f6cdbed9d4d2f057
Ocen Chris <43829976+chrisjrocen@users.noreply.github.com>
2022-04-19 20:40:52 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -4,7 +4,7 @@
  * _printf - formatted output conversion and print data.
  * @format: input string.
  *
- * Return: number of chars printed.
+ * Return: the number of chars printed.
  */
 int _printf(const char *format, ...)
 {
