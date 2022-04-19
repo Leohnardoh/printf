025388b65561b8cbe29be5ba551f5ef009d00025
Ocen Chris <43829976+chrisjrocen@users.noreply.github.com>
2022-04-19 21:06:00 +0300

    Update print_chr.c

--- print_chr.c
+++ print_chr.c
@@ -5,7 +5,7 @@
  * @arguments: input char
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
- * Return: On success 1.
+ * Return: On success - 1.
  */
 int print_chr(va_list arguments, char *buf, unsigned int ibuf)
 {
