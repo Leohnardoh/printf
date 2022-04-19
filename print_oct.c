dbd64246c064c107bb3f3a522c03b9bff2af607c
Ocen Chris <43829976+chrisjrocen@users.noreply.github.com>
2022-04-19 20:52:55 +0300

    Update print_oct.c

--- print_oct.c
+++ print_oct.c
@@ -5,7 +5,7 @@
  * @arguments: input number
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
- * Return: number of chars printed.
+ * Return: the number of chars printed.
  */
 int print_oct(va_list arguments, char *buf, unsigned int ibuf)
 {
