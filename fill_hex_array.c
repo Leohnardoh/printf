c178579095334257795bf35ac50d14a8757321c0
Ocen Chris <43829976+chrisjrocen@users.noreply.github.com>
2022-04-19 20:44:12 +0300

    Update fill_hex_array.c

--- fill_hex_array.c
+++ fill_hex_array.c
@@ -7,7 +7,7 @@
  * @hex: array where is stored the hexadecimal.
  * @isupp: integer that determines if the hexadecimal array is
  * in uppercase or lowercase letter.
- * @limit: size of hex
+ * @limit: size of hex.
  * Return: binary array.
  */
 char *fill_hex_array(char *bnr, char *hex, int isupp, int limit)
